7b6c69635fb5fb97b6f576d22167f1f1882c12e0
Diyansh Rao <87599592+DiyanshRao@users.noreply.github.com>
2023-11-16 15:05:33 +0530

    navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,22 @@
+import './styles.css'
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='nav'>
+      <div>
+        <span className='nav-text'>Vendors || Exploring your city </span>
+      </div>
+      <div className='flexing '>
+        <a href='/' className='nav-text2'>
+          Home
+        </a>
+        <a href='/explore' className='nav-text2'>
+          Explore
+        </a>
+        <a href='/map' className='nav-text2'>
+          Map
+        </a>
+      </div>
+    </div>
+  )
 }
 export default Navbar
