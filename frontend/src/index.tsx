567766d7c194458256495e12ed1e2a81749d0b66
Diyansh Rao <87599592+DiyanshRao@users.noreply.github.com>
2023-11-16 14:05:46 +0530

    changes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import Navbar from '@components/Navbar'
+import Navbar from './components/Navbar'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
